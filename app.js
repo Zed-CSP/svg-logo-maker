e430155847066d0fee0c47ebf909875de416a2df
Christopher Peret <cperet89@gmail.com>
2023-06-25 17:22:23 -0700

    restructure questions to constants.js

--- app.js
+++ app.js
@@ -2,73 +2,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const constructors = require('./assets/constructors.js');
+const questions = require('./lib/constants.js');
 
 
 
-// array of questions for user
-const questions = [
-    {
-        type: 'input',
-        name: 'logoname',
-        message: 'enter 3 Logo Letters',
-        validate: function (answer) {
-            if (answer.length > 3) {
-                return console.log("Please enter a maximum of 3 characters.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        name: 'text-color',
-        message: 'enter text color',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("Please enter a color.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        name: 'background-color',
-        message: 'enter background color',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("Please enter a color.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        name: 'font',
-        message: 'enter font',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("Please enter a font.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'enter shape',
-        choices: ['square', 'circle', 'triangle'],
-    },
-    {
-        type: 'input',
-        name: 'shape-color',
-        message: 'enter shape color',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("Please enter a color.");
-            }
-            return true;
-        }
-    },
-];
+
 
 // function to write svg file
 function writeToFile(fileName, data) {
@@ -87,7 +25,7 @@ function init() {
         const font = answers.font;
         const shape = answers.shape;
         const shapeColor = answers['shape-color'];
-        
+
 
         // create svg
         writeToFile('logo.svg', svg);
